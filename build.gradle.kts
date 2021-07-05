06a02399b28851eef7ee655a1da367a544ed3298
jaychy-yy <0816jinjin@gmail.com>
2021-07-05 17:17:22 +0900

    feat: 오셀로 알 설치 로직 구현 완료.

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.30"
+    id("org.jetbrains.compose") version "0.3.1"
+}
+
+group = "com.j"
+version = "1.0"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "test"
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,133 @@
+import androidx.compose.desktop.Window
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+typealias OthelloGameBoard = Array<CharArray>
+
+fun main() = Window(
+    title = "Othello Game",
+    size = IntSize(640, 640),
+) {
+    var othelloGameBoard by remember {
+        mutableStateOf(
+            arrayOf(
+                charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+                charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+                charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+                charArrayOf('_', '_', '_', '○', '●', '_', '_', '_'),
+                charArrayOf('_', '_', '_', '●', '○', '_', '_', '_'),
+                charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+                charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+                charArrayOf('_', '_', '_', '_', '_', '_', '_', '_'),
+            )
+        )
+    }
+    var blackTurn by remember { mutableStateOf(true) }
+
+    MaterialTheme {
+        othelloGameBoard.mapIndexed { rowIndex, rowValue ->
+            Row(
+                modifier = Modifier.absoluteOffset(y = (rowIndex * 75).dp)
+            ) {
+                rowValue.mapIndexed { columnIndex, columnValue ->
+                    Button(
+                        onClick = {
+                            othelloGameBoard = getChangedOthelloGameBoard(
+                                gameBoard = othelloGameBoard,
+                                rowIndex = rowIndex,
+                                columnIndex = columnIndex,
+                                team = if (blackTurn) OthelloTeam.BLACK else OthelloTeam.WHITE
+                            )
+                            blackTurn = blackTurn.turnEnd()
+                        },
+                        modifier = Modifier.weight(4F)
+                            .fillMaxWidth(0.125F)
+                            .fillMaxHeight(0.125F),
+                        colors = ButtonDefaults.textButtonColors(backgroundColor = Color.White)
+                    ) {
+                        Text(
+                            text = columnValue.toString(),
+                            fontSize = 50.sp,
+                            color = Color.Black,
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+private fun getChangedOthelloGameBoard(
+    gameBoard: OthelloGameBoard,
+    rowIndex: Int,
+    columnIndex: Int,
+    team: OthelloTeam,
+) = arrayOf(
+    createBoardRow(gameBoard, rowIndex, columnIndex, team, OthelloIndex.ZERO),
+    createBoardRow(gameBoard, rowIndex, columnIndex, team, OthelloIndex.ONE),
+    createBoardRow(gameBoard, rowIndex, columnIndex, team, OthelloIndex.TWO),
+    createBoardRow(gameBoard, rowIndex, columnIndex, team, OthelloIndex.THREE),
+    createBoardRow(gameBoard, rowIndex, columnIndex, team, OthelloIndex.FOUR),
+    createBoardRow(gameBoard, rowIndex, columnIndex, team, OthelloIndex.FIVE),
+    createBoardRow(gameBoard, rowIndex, columnIndex, team, OthelloIndex.SIX),
+    createBoardRow(gameBoard, rowIndex, columnIndex, team, OthelloIndex.SEVEN),
+)
+
+private fun createBoardRow(
+    gameBoard: OthelloGameBoard,
+    rowIndex: Int,
+    columnIndex: Int,
+    team: OthelloTeam,
+    targetRowIndex: OthelloIndex,
+) = if (rowIndex == targetRowIndex.index) {
+    val a = charArrayOf(
+        createBoardElement(gameBoard, rowIndex, columnIndex, team, targetRowIndex, OthelloIndex.ZERO),
+        createBoardElement(gameBoard, rowIndex, columnIndex, team, targetRowIndex, OthelloIndex.ONE),
+        createBoardElement(gameBoard, rowIndex, columnIndex, team, targetRowIndex, OthelloIndex.TWO),
+        createBoardElement(gameBoard, rowIndex, columnIndex, team, targetRowIndex, OthelloIndex.THREE),
+        createBoardElement(gameBoard, rowIndex, columnIndex, team, targetRowIndex, OthelloIndex.FOUR),
+        createBoardElement(gameBoard, rowIndex, columnIndex, team, targetRowIndex, OthelloIndex.FIVE),
+        createBoardElement(gameBoard, rowIndex, columnIndex, team, targetRowIndex, OthelloIndex.SIX),
+        createBoardElement(gameBoard, rowIndex, columnIndex, team, targetRowIndex, OthelloIndex.SEVEN),
+    )
+    println(a)
+    a
+}
+else {
+    val a = gameBoard[targetRowIndex.index]
+    println(a)
+    a
+}
+
+private fun createBoardElement(
+    gameBoard: OthelloGameBoard,
+    rowIndex: Int,
+    columnIndex: Int,
+    team: OthelloTeam,
+    targetRowIndex: OthelloIndex,
+    targetColumnIndex: OthelloIndex,
+) = if (columnIndex == targetColumnIndex.index) team.shape else gameBoard[targetRowIndex.index][targetColumnIndex.index]
+
+enum class OthelloTeam(val shape: Char) {
+    BLACK('●'),
+    WHITE('○'),
+}
+
+enum class OthelloIndex(val index: Int) {
+    ZERO(0),
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+}
+
+fun Boolean.turnEnd() = !this
