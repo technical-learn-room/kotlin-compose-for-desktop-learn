4eff299736e907d8bfb55eb10fd443f62da87fe1
jaychy-yy <0816jinjin@gmail.com>
2021-07-06 10:42:56 +0900

    feat: 종료 로직 구현 완료.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -20,8 +20,10 @@ import androidx.compose.ui.unit.sp
 typealias OthelloGameBoard = Array<CharArray>
 typealias GameBoardColors = Array<Array<Color>>
 
+var title = "Othello Game"
+
 fun main() = Window(
-    title = "Othello Game",
+    title = title,
     size = IntSize(640, 640),
 ) {
     var othelloGameBoard by remember {
@@ -44,27 +46,46 @@ fun main() = Window(
                 rowValue.mapIndexed { columnIndex, columnValue ->
                     Button(
                         onClick = {
-                            othelloGameBoard = insertPointInOthelloGameBoard(
-                                gameBoard = othelloGameBoard,
-                                rowIndex = rowIndex,
-                                columnIndex = columnIndex,
-                                team = blackTurn.team
-                            )
-                            blackTurn = blackTurn.turnEnd()
+                            if (othelloGameBoardColors[rowIndex][columnIndex] == Color.Yellow) {
+                                othelloGameBoard = insertPointInOthelloGameBoard(
+                                    gameBoard = othelloGameBoard,
+                                    rowIndex = rowIndex,
+                                    columnIndex = columnIndex,
+                                    team = blackTurn.team,
+                                )
+                                blackTurn = blackTurn.turnEnd()
 
-                            applyChainAction(
-                                gameBoard = othelloGameBoard,
-                                rowIndex = rowIndex,
-                                columnIndex = columnIndex,
-                            )
+                                applyChainAction(
+                                    gameBoard = othelloGameBoard,
+                                    rowIndex = rowIndex,
+                                    columnIndex = columnIndex,
+                                )
 
-                            highlightAvailablePoint(
-                                gameBoard = othelloGameBoard,
-                                gameBoardColors = othelloGameBoardColors,
-                                team = blackTurn.team
-                            )
+                                highlightAvailablePoint(
+                                    gameBoard = othelloGameBoard,
+                                    gameBoardColors = othelloGameBoardColors,
+                                    team = blackTurn.team,
+                                )
 
-                            othelloGameBoard.checkEndGame()
+                                if (othelloGameBoardColors.checkEndGame()) {
+                                    val blackCount = othelloGameBoard.count(OthelloTeam.BLACK)
+                                    val whiteCount = othelloGameBoard.count(OthelloTeam.WHITE)
+
+                                    when {
+                                        blackCount > whiteCount -> {
+                                            title = "Winner is Black"
+                                            othelloGameBoard.end(OthelloTeam.BLACK)
+                                        }
+                                        blackCount < whiteCount -> {
+                                            title = "Winner is White"
+                                            othelloGameBoard.end(OthelloTeam.WHITE)
+                                        }
+                                        else -> {
+                                            othelloGameBoard.end(OthelloTeam.ANONYMOUS)
+                                        }
+                                    }
+                                }
+                            }
                         },
                         modifier = Modifier.weight(4F)
                             .fillMaxWidth(0.125F)
@@ -85,11 +106,28 @@ fun main() = Window(
 
 fun Boolean.turnEnd() = !this
 
-fun OthelloGameBoard.checkEndGame() =
-    this.count(OthelloTeam.ANONYMOUS)
+fun GameBoardColors.checkEndGame() =
+    this.sumOf { row -> row.filter { it == Color.Yellow }.count() } <= 0
 
-private fun OthelloGameBoard.count(matchCharacter: OthelloTeam) =
+fun OthelloGameBoard.count(matchCharacter: OthelloTeam) =
     this.sumOf { row -> row.filter { it == matchCharacter.shape }.count() }
 
 val Boolean.team: OthelloTeam
     get() = if (this) OthelloTeam.BLACK else OthelloTeam.WHITE
+
+fun OthelloGameBoard.end(winner: OthelloTeam) {
+    when (winner) {
+        OthelloTeam.BLACK, OthelloTeam.WHITE ->
+            this.forEachIndexed { rowIndex, rowValue ->
+                rowValue.forEachIndexed { columnIndex, _ ->
+                    this[rowIndex][columnIndex] = winner.shape
+                }
+            }
+        OthelloTeam.ANONYMOUS ->
+            this.forEachIndexed { rowIndex, rowValue ->
+                rowValue.forEachIndexed { columnIndex, _ ->
+                    this[rowIndex][columnIndex] = if (rowIndex % 2 == 0) OthelloTeam.BLACK.shape else OthelloTeam.WHITE.shape
+                }
+            }
+    }
+}
